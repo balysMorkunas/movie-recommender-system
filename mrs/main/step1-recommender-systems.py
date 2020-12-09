438e602a77cf11dad23296711557334aeddf8078
Balys Morkunas <balys.morkunas@gmail.com>
2020-12-09 10:49:16 +0200

    Implement central cosine distance

--- mrs/main/step1-recommender-systems.py
+++ mrs/main/step1-recommender-systems.py
@@ -16,19 +16,24 @@ To know more about the expectations, please refer to the guidelines.
 ##
 #####
 
-#Where data is located
+# Where data is located
 movies_file = '../data/movies.csv'
 users_file = '../data/users.csv'
 ratings_file = '../data/ratings.csv'
 predictions_file = '../data/predictions.csv'
 submission_file = '../data/submission.csv'
 
-
 # Read the data using pandas
-movies_description = pd.read_csv(movies_file, delimiter=';', dtype={'movieID':'int', 'year':'int', 'movie':'str'}, names=['movieID', 'year', 'movie'])
-users_description = pd.read_csv(users_file, delimiter=';', dtype={'userID':'int', 'gender':'str', 'age':'int', 'profession':'int'}, names=['userID', 'gender', 'age', 'profession'])
-ratings_description = pd.read_csv(ratings_file, delimiter=';', dtype={'userID':'int', 'movieID':'int', 'rating':'int'}, names=['userID', 'movieID', 'rating'])
+movies_description = pd.read_csv(movies_file, delimiter=';', dtype={'movieID': 'int', 'year': 'int', 'movie': 'str'},
+                                 names=['movieID', 'year', 'movie'])
+users_description = pd.read_csv(users_file, delimiter=';',
+                                dtype={'userID': 'int', 'gender': 'str', 'age': 'int', 'profession': 'int'},
+                                names=['userID', 'gender', 'age', 'profession'])
+ratings_description = pd.read_csv(ratings_file, delimiter=';',
+                                  dtype={'userID': 'int', 'movieID': 'int', 'rating': 'int'},
+                                  names=['userID', 'movieID', 'rating'])
 predictions_description = pd.read_csv(predictions_file, delimiter=';', names=['userID', 'movieID'], header=None)
+
 
 #####
 ##
@@ -42,18 +47,47 @@ def predict_collaborative_filtering(movies, users, ratings, predictions):
     pass
 
 
+def construct_utility_matrix(matrixA, matrixB):
+    """
+    Constructs a Utility Matrix from two matrices.
+    :param matrixA: DataFrame
+    :param matrixB: DataFrame
+    :return: DataFrame
+    """
+
+    pass
+
+
+def central_cosine_distance(vecA, vecB):
+    """
+    Calculates Pearsons Correlation between two vectors.
+    :param vecA: list
+    :param vecB: list
+    :return: float
+    """
+    meanA = np.mean(vecA)
+    meanB = np.mean(vecB)
+    sum_numerator = np.sum((vecA - meanA) * (vecB - meanB))
+    sum_denominator = np.sqrt(np.sum((vecA - meanA) ** 2)) * np.sqrt(np.sum((vecB - meanB) ** 2))
+
+    return sum_numerator/sum_denominator
+
+print(central_cosine_distance([2, 0, 0, 1, 3],
+                              [1, 0, 2, 3, 0]))
+
+
 #####
 ##
 ## LATENT FACTORS
 ##
 #####
-    
+
 def predict_latent_factors(movies, users, ratings, predictions):
     ## TO COMPLETE
 
     pass
-    
-    
+
+
 #####
 ##
 ## FINAL PREDICTORS
@@ -61,9 +95,9 @@ def predict_latent_factors(movies, users, ratings, predictions):
 #####
 
 def predict_final(movies, users, ratings, predictions):
-  ## TO COMPLETE
+    ## TO COMPLETE
 
-  pass
+    pass
 
 
 #####
@@ -72,12 +106,13 @@ def predict_final(movies, users, ratings, predictions):
 ## //!!\\ TO CHANGE
 ##
 #####
-    
-#By default, predicted rate is a random classifier
+
+# By default, predicted rate is a random classifier
 def predict_random(movies, users, ratings, predictions):
     number_predictions = len(predictions)
 
     return [[idx, randint(1, 5)] for idx in range(1, number_predictions + 1)]
+
 
 #####
 ##
@@ -88,12 +123,12 @@ def predict_random(movies, users, ratings, predictions):
 ## //!!\\ TO CHANGE by your prediction function
 predictions = predict_random(movies_description, users_description, ratings_description, predictions_description)
 
-#Save predictions, should be in the form 'list of tuples' or 'list of lists'
+# Save predictions, should be in the form 'list of tuples' or 'list of lists'
 with open(submission_file, 'w') as submission_writer:
-    #Formates data
+    # Formates data
     predictions = [map(str, row) for row in predictions]
     predictions = [','.join(row) for row in predictions]
-    predictions = 'Id,Rating\n'+'\n'.join(predictions)
-    
-    #Writes it dowmn
+    predictions = 'Id,Rating\n' + '\n'.join(predictions)
+
+    # Writes it dowmn
     submission_writer.write(predictions)
