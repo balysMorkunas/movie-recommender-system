f3319f5ea020ff48a4899fb7d691691721a785ba
Balys Morkunas <balys.morkunas@gmail.com>
2021-01-18 21:46:33 +0200

    Add LF implementation with ALS, SGD, SGDwb

--- mrs/main/step1-recommender-systems.py
+++ mrs/main/step1-recommender-systems.py
@@ -3,7 +3,10 @@ import pandas as pd
 import os.path
 from random import randint
 from tqdm import tqdm
-from sklearn.metrics import mean_squared_error
+import matplotlib
+import matplotlib.pyplot as plt
+import uuid
+import time
 
 # -*- coding: utf-8 -*-
 """
@@ -50,13 +53,11 @@ predictions_description = pd.read_csv(predictions_file, delimiter=';',
 # meaning some movies were not rated by anyone.
 # We fix this by joining movies dataframe with
 # ratings dataframe and filling the nan values with 0's.
-ratings_test = pd.DataFrame(np.array([
-                [0, 0, 1, 2, 3, 0, 0, 1, 0, 2, 4, 1, 5, 2],
-                [0, 0, 0, 0, 0, 4, 2, 1, 4, 0, 0, 0, 2, 2],
-                [1, 0, 0, 2, 4, 0, 0, 0, 0, 1, 4, 0, 0, 2],
-                [0, 1, 4, 0, 0, 0, 2, 1, 2, 0, 0, 0, 1, 1],
-                [4, 0, 0, 0, 0, 0, 0, 0, 1, 4, 2, 1, 2, 0],
-                [0, 5, 1, 0, 0, 1, 2, 1, 1, 1, 0, 0, 0, 2]]))
+ratings = (movies_description.set_index("movieID")).join(
+    ratings_description.pivot(index='movieID', columns='userID', values='rating'))
+ratings.drop(['year', 'movie'], axis=1, inplace=True)
+ratings = ratings.fillna(0)
+
 #####
 ##
 # COLLABORATIVE FILTERING
@@ -172,28 +173,131 @@ def predict_rating(s, r):
 ##
 #####
 
+#-------------------------------------------------Util---------------------------------------------------
+def grid_search(samples, R, b, steps=500, gamma=0.01, lamda=0.01, rmse = False, bias = False):
+    """
+    Perfomrs grid search to find the best pair of hyper paramters k and learning_rate
+    """
 
-def get_mse(pred, actual):
-    # Ignore nonzero terms.
-    pred = pred[actual.nonzero()].flatten()
-    actual = actual[actual.nonzero()].flatten()
-    return mean_squared_error(pred, actual)
+    ks = [25, 30]
+    learn_rates = [0.01, 0.001, 0.0001]
+
+    for k in ks:
+        P = np.random.normal(scale=1./k, size=(len(R), k))
+        Q = np.random.normal(scale=1./k, size=(len(R[0]), k))
+        b_u = np.zeros(len(R))
+        b_i = np.zeros(len(R[0]))
+        for lr in learn_rates:
+            newP, newQ, newB_u, newB_i = matrix_factr(samples, R, P, Q, b, b_u, b_i, gamma=lr, rmse=True, bias=True)
+            rmse_score = get_rmse(R, newP, newQ, b, newB_u, newB_i, bias=True)
+            with open('grid_search.txt', 'a') as f:
+                print(f"k={k}, lr={lr}, rmse={rmse_score}", file=f)
 
 
-def train_test_split(ratings):
+def plot_rmse(mse):
+    """
+    Plots the mean squared error over all steps
+    :param mse:
+    :return:
+    """
+    indeces = [i for i, j in mse]
+    mses = [j for i, j in mse]
+    with open('mse_bias_100s.txt', 'w') as f:
+        print(mses, file=f)
+    plt.figure(figsize=((16,4)))
+    plt.plot(indeces, mses)
+    plt.xticks(indeces, indeces)
+    plt.xlabel("Steps")
+    plt.ylabel("RMSE")
+    plt.grid(axis="y")
+    filename = time.strftime("%Y%m%d-%H%M%S")
+    plt.savefig(filename + '.png')
 
-    # We want to take matrix R
-    # select ~ 900 users (rows) that have rated more than 10
-    # movies and put them into the test set the values we've put should be changed to 0
-    # but the row should not be left with 0 ratings!!!
-    # all the rows we took out should be predicted
+
+def get_rmse(R, P, Q, b, b_u, b_i, bias):
+    """
+    Computes total mean squared error
+    :param R:
+    :param P:
+    :param Q:
+    :return:
+    """
+    i_indeces, j_indeces = R.nonzero()
+    pred = predict_all(P, Q, b, b_u, b_i, bias) 
+    e = 0
+    for i, j in zip(i_indeces, j_indeces):
+        e += pow(R[i][j] - pred[i][j], 2)
+    return np.sqrt(e/len(i_indeces))
 
 
+def predict_single(i, j, P, Q, b, b_u, b_i, bias = False):
+    """
+    Predicts the rating for a single user i and item j
+    """
+    if bias:
+        return (b + b_u[i] + b_i[j] + np.dot(P[i, :], Q[:, j]))
+    else:
+        return np.dot(P[i, :], Q[:, j])
 
-    return train, test
+
+def predict_all(P, Q, b, b_u, b_i, bias = False):
+    """
+    Return the whole prediction matrix
+    """
+    if bias:
+        return b + b_u[:, np.newaxis] + b_i[np.newaxis:, ] + np.dot(P, Q)
+    else:
+        return np.dot(P, Q)
 
 
-def matrix_factr(R, P, Q, ks, steps=100, gamma=0.0002, lamda=0.02):
+#-----------------------------------------------ALS-----------------------------------------------------------
+
+def als_step(R, update_matrix, fixed_matrix, k, gamma):
+    """
+    In ALS we update one matrix while the other stays the same
+    """
+    A = fixed_matrix.T.dot(fixed_matrix) + np.eye(k) * gamma
+    b = R.dot(fixed_matrix)
+    A_inv = np.linalg.inv(A)
+    update_matrix = b.dot(A_inv)
+
+    return update_matrix
+
+
+def matrix_als(R, P, Q, k, steps=100, gamma=0.01, rmse = False, bias = False):
+    """
+    Alternating Least Sqaures method for optimizing
+    P and Q matrices.
+    """
+    rmse_data = []
+    rmse_old = get_rmse(R, P, Q.T, 0, [], [], False)
+
+    for step in tqdm(range(steps)):
+        Q[Q < 0] = 0
+        P = als_step(R, P, Q, k, gamma)
+        P[P < 0] = 0
+        Q = als_step(R.T, Q, P, k, gamma)
+
+        if rmse:
+            rmse_new = get_rmse(R, P, Q.T, 0, [], [], False)
+            print(rmse_new)
+            # Check for convergence and break if new RMSE is bigger than old
+            if np.abs(rmse_new - rmse_old) < 0.0001:
+                rmse_data.append((step + 1, rmse_new))
+                break
+            else:
+                rmse_data.append((step + 1, rmse_new))
+                rmse_old = rmse_new
+    if rmse:
+        plot_rmse(rmse_data)
+
+    return P, Q
+
+
+#-----------------------------------------------SDG-----------------------------------------------------------
+
+
+def matrix_factr(samples, R, P, Q, b, b_u, b_i, steps=300, gamma=0.01, lamda=0.01, rmse = False, bias = False):
     '''
     R: Ratings matrix
     P: |users| * k - user feature matrix
@@ -201,86 +305,146 @@ def matrix_factr(R, P, Q, ks, steps=100, gamma=0.0002, lamda=0.02):
     ks: number of latent features we want
     steps: iterations
     gamma: learning rate
-    lamda: regularization rate
+    lamda: regularization rate - bigger rate means less overfitting
     '''
 
     Q = Q.T
+    rmse_data = []
+    rmse_old = get_rmse(R, P, Q, b, b_u, b_i, bias)
+    rmse_data.append((0, rmse_old))
 
     for step in tqdm(range(steps)):
-        for i in range(len(R)):
-            for j in range(len(R[i])):
-                if R[i, j] > 0:
-                    # Error calculation
-                    error_i_j = R[i, j] - np.dot(P[i, :], Q[:, j])
+        np.random.shuffle(samples)
+        for i, j, r in samples:
+            # Error calculation
+            e = r - predict_single(i, j, P, Q, b, b_u, b_i, bias)
+            # Bias calculation
+            if bias:
+                b_u[i] += gamma * (e - lamda * b_u[i])
+                b_i[j] += gamma * (e - lamda * b_i[j])
+            # Gradient calculation
+            p_old = P[i, :][:]
+            P[i,:] += gamma * (e * Q[:,j] - lamda * P[i,:])
+            Q[:,j] += gamma * (e * p_old - lamda * Q[:,j])
 
-                    for k in range(ks):
-                        # Calculate the gradient
-                        P[i][k] = P[i][k] + gamma * (2 * error_i_j * Q[k][j] - lamda * P[i][k])
-                        Q[k][j] = Q[k][j] + gamma * (2 * error_i_j * P[i][k] - lamda * Q[k][j])
+        if (step + 1) % 10 == 0 and rmse:
+            rmse_new = get_rmse(R, P, Q, b, b_u, b_i, bias)
+            # Check for convergence and break if new RMSE is bigger than old
+            if np.abs(rmse_new - rmse_old) < 0.0001:
+                rmse_data.append((step + 1, rmse_new))
+                break
+            else:
+                rmse_data.append((step + 1, rmse_new))
+                rmse_old = rmse_new
+    if rmse:
+        plot_rmse(rmse_data)
 
-        # Check for convergence
-    #    e = 0
-    #    for i in range(len(R)):
-    #        printProgressBar(i, len(R), suffix=' 2. Convergence')
-    #        for j in range(len(R[i])):
-    #            if R[i, j] > 0:
-    #                e = e + pow(R[i][j] - np.dot(P[i, :], Q[:, j]), 2)
+    return P, Q, b_u, b_i
 
-    #                for k in range(ks):
-    #                    e = e + (lamda/2) * (pow(P[i][k], 2) + pow(Q[k][j], 2))
-    #    print(f'e = {e}')
-
-    #    if e < 0.001:
-    #        break
-
-    return P, Q
+#-----------------------------------------------Prediction functions for LF-----------------------------------------------------------
 
 
-def predict_latent_factors(movies, users, ratings, predictions):
-
-    ratings = (movies_description.set_index("movieID")).join(
-        ratings.pivot(index='movieID', columns='userID', values='rating'))
-    ratings.drop(['year', 'movie'], axis=1, inplace=True)
-    ratings = ratings.fillna(0)
+def predict_latent_factors_with_bias(movies, users, ratings, predictions):
+    # R: Ratings matrix
     R = ratings.to_numpy()
-    # R = ratings_test.to_numpy()
-
-    # First we need to split our data to train and test set!
-    trainR, testR = train_test_split(R)
-
     # n: number of users
-    n = len(trainR)
+    n = len(R)
     # m: number of movies
-    m = len(trainR[0])
+    m = len(R[0])
     # k: number of features
-    k = 4
+    k = 25
+    # bias: do we want to include bias in our computation
+    bias_value = True
 
-    P = np.random.rand(n, k)
-    Q = np.random.rand(m, k)
+    # Initialise random P and Q matrices
+    P = np.random.normal(scale=1./k, size=(n, k))
+    Q = np.random.normal(scale=1./k, size=(m, k))
 
-    #u, s, vh = np.linalg.svd(R, full_matrices=False)
+    # Initialise biases
+    b = np.mean(R[np.where(R != 0)])
+    b_u = np.zeros(n)
+    b_i = np.zeros(m)
 
-    #print(np.shape(u)) # reduce columns 
-    #print(np.shape(vh)) #reduce rows
-    #Q = u[:, :k]
-    #VT = vh[:k, :]
-    #s = s[:k]
-    #s = np.diag(s)
-    #P = np.dot(s, VT)
-    #print(np.shape(Q))
-    #print(np.shape(P))
+    # Throw away 0 rated entries once instead
+    # of checking every step if R[i][j] is 0
+    samples = [
+        (i, j, R[i, j])
+        for i in range(n)
+        for j in range(m)
+        if R[i][j] > 0
+    ]
 
-    # newP, newQ = matrix_factr(trainR, P, Q, k)
+    newP, newQ, newB_u, newB_i = matrix_factr(samples, R, P, Q, b, b_u, b_i, rmse=True, bias=bias_value)
+    # newP, newQ = matrix_als(R, P, Q, k, rmse=True, bias=bias_value)
 
-   # newR = np.dot(newP, newQ)
-   # number_predictions = len(predictions)
-   # result = [[idx+1, newR[predictions.movieID[idx]-1, predictions.userID[idx]-1]] for idx in range(0, number_predictions)]
-    result = 0
+    newR = predict_all(newP, newQ, b, newB_u, newB_i, bias=bias_value)
+    # newR = predict_all(newP, newQ.T, b, b_u, b_i)
+
+    number_predictions = len(predictions)
+    result = [[idx+1, newR[predictions.movieID[idx]-1, predictions.userID[idx]-1]] for idx in range(0, number_predictions)]
     return result
 
 
-#print(predict_latent_factors(movies_description, users_description, ratings_description, predictions_description))
+def predict_latent_factors_no_bias(movies, users, ratings, predictions):
+    # R: Ratings matrix
+    R = ratings.to_numpy()
+    # n: number of users
+    n = len(R)
+    # m: number of movies
+    m = len(R[0])
+    # k: number of features
+    k = 25
+    # bias: do we want to include bias in our computation
+    bias_value = False
 
+    # Initialise random P and Q matrices
+    P = np.random.normal(scale=1./k, size=(n, k))
+    Q = np.random.normal(scale=1./k, size=(m, k))
+
+    # Initialise biases
+    b = np.mean(R[np.where(R != 0)])
+    b_u = np.zeros(n)
+    b_i = np.zeros(m)
+
+    # Throw away 0 rated entries once instead
+    # of checking every step if R[i][j] is 0
+    samples = [
+        (i, j, R[i, j])
+        for i in range(n)
+        for j in range(m)
+        if R[i][j] > 0
+    ]
+
+    newP, newQ, newB_u, newB_i = matrix_factr(samples, R, P, Q, b, b_u, b_i, rmse=True, bias=bias_value)
+    newR = predict_all(newP, newQ, b, newB_u, newB_i, bias=bias_value)
+
+    number_predictions = len(predictions)
+    result = [[idx+1, newR[predictions.movieID[idx]-1, predictions.userID[idx]-1]] for idx in range(0, number_predictions)]
+    return result
+
+
+def predict_latent_factors_ALS(movies, users, ratings, predictions):
+    # R: Ratings matrix
+    R = ratings.to_numpy()
+    # n: number of users
+    n = len(R)
+    # m: number of movies
+    m = len(R[0])
+    # k: number of features
+    k = 25
+    # bias: do we want to include bias in our computation
+
+    # Initialise random P and Q matrices
+    P = np.random.normal(scale=1./k, size=(n, k))
+    Q = np.random.normal(scale=1./k, size=(m, k))
+
+    newP, newQ = matrix_als(R, P, Q, k, rmse=True)
+
+    newR = predict_all(newP, newQ.T, b, b_u, b_i)
+
+    number_predictions = len(predictions)
+    result = [[idx+1, newR[predictions.movieID[idx]-1, predictions.userID[idx]-1]] for idx in range(0, number_predictions)]
+    return result
 
 #####
 ##
@@ -295,19 +459,6 @@ def predict_final(movies, users, ratings, predictions):
     pass
 
 
-#####
-##
-# RANDOM PREDICTORS
-# //!!\\ TO CHANGE
-##
-#####
-
-# By default, predicted rate is a random classifier
-def predict_random(movies, users, ratings, predictions):
-    number_predictions = len(predictions)
-
-    return [[idx, randint(1, 5)] for idx in range(1, number_predictions + 1)]
-
 
 #####
 ##
@@ -317,7 +468,7 @@ def predict_random(movies, users, ratings, predictions):
 
 
 # //!!\\ TO CHANGE by your prediction function
-predictions = predict_latent_factors(movies_description, users_description, ratings_description, predictions_description)
+predictions = predict_latent_factors_with_bias(movies_description, users_description, ratings, predictions_description)
 
 # Save predictions, should be in the form 'list of tuples' or 'list of lists'
 with open(submission_file, 'w') as submission_writer:
